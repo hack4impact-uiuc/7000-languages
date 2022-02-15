8db6f690e0c2b514909a2a9dd4e88aca8c8a1a85
nroy20 <nroy20@illinois.edu>
2022-02-14 19:48:00 -0600

    Remove first and last names

--- api/src/models/user.js
+++ api/src/models/user.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 const Home = new mongoose.Schema({
   role: { type: Number, required: true },
   authID: { type: String, required: true },
-  firstName: { type: String, required: false, default: '' },
-  lastName: { type: String, required: false, default: '' },
   adminLanguages: { type: [String], required: false, default: [] },
   learnerLanguags: { type: [String], required: false, default: [] },
   collaboratorLanguages: { type: [String], required: false, default: [] },
