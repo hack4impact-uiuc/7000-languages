d585f2972da0b39883faf9e3ca5de5beffd97412
nroy20 <nroy20@illinois.edu>
2022-02-14 21:01:52 -0600

    Test post request

--- api/src/api/index.js
+++ api/src/api/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => res.json('API working!'));
+router.post('/', (req, res) => res.json('API working!!'));
 // Put all routes here
 router.use('/home', require('./home'));
 
