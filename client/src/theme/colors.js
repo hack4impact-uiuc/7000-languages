9b1378e606e8a80c09adf89d0bedff55dabdde53
Ashay Parikh <ashayparikh22@gmail.com>
2022-02-15 00:12:59 -0600

    style

--- client/src/theme/colors.js
+++ client/src/theme/colors.js
@@ -1,30 +1,30 @@
 // 7000 Languages Primary Identity Colors
 const colors = {
   red: {
-    light: '#F9EDED',  // 100 opacity
+    light: '#F9EDED', // 100 opacity
     medium: '#E9BAB6', // 300 opacity
-    dark: '#CE584E',   // 500 opacity
+    dark: '#CE584E', // 500 opacity
   },
   blue: {
-    light: '#E9F6F7',  // 100 opacity
+    light: '#E9F6F7', // 100 opacity
     medium: '#AADDDF', // 300 opacity
-    dark: '#4FACB0',   // 500 opacity
+    dark: '#4FACB0', // 500 opacity
   },
   orange: {
-    light: '#FFF1DD',  // 100 opacity
+    light: '#FFF1DD', // 100 opacity
     medium: '#FFC989', // 300 opacity
-    dark: '#E59A41',   // 500 opacity
+    dark: '#E59A41', // 500 opacity
   },
   green: {
-    light: '#F6FBD4',   // 100 opacity
-    medium: '#D8E48B',  // 300 opacity
-    dark: '#C0D152',    // 500 opacity
+    light: '#F6FBD4', // 100 opacity
+    medium: '#D8E48B', // 300 opacity
+    dark: '#C0D152', // 500 opacity
   },
   gray: {
-    light: '#EFEFEF',         // 100 opacity
-    medium_light: '#A8AFB4',  // 300 opacity
-    medium: '#5B6165',        // 500 opacity
-    dark: '#060606',          // 800 opacity
+    light: '#EFEFEF', // 100 opacity
+    medium_light: '#A8AFB4', // 300 opacity
+    medium: '#5B6165', // 500 opacity
+    dark: '#060606', // 800 opacity
   },
 }
 
