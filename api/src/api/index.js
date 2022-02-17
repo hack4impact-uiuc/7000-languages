97219bed4327bfcfa3e86f5426f21d79e9b95059
nroy20 <nroy20@illinois.edu>
2022-02-17 16:15:17 -0600

    Write GET endpoint for testing purposes

--- api/src/api/index.js
+++ api/src/api/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => res.json('API working!'));
-router.post('/', (req, res) => res.json('API working!!'));
+router.post('/users', (req, res) => res.json('API working!!'));
+router.get('/users', (req, res) => res.json('API working!!!'));
 // Put all routes here
 router.use('/home', require('./home'));
 
--- api/src/api/user.js
+++ api/src/api/user.js
@@ -5,16 +5,37 @@ const { sendResponse } = require('../utils/response');
 const { models } = require('../models/index.js')
 
 router.post(
-  '/',
+  '/users',
   errorWrap(async (req, res) => {
     const userInfo = req.body;
-    const newUser = new models.User(userInfo);
+    const newUser = new models.User({
+      role: userInfo.role,
+      authID: userInfo.authID,
+      adminLanguages: userInfo.adminLanguages,
+      learnerLanguages: userInfo.learnerLanguages,
+      collaboratorLanguages: userInfo.collaboratorLanguages
+    });
     await newUser.save();
     return sendResponse(
       res,
       200,
       'Successfully created a new user',
       newUser,
+    );
+  }),
+);
+
+//testing only!
+router.get(
+  '/users',
+  errorWrap(async (req, res) => {
+    const successMessage =
+      "u did a get c:";
+    return sendResponse(
+      res,
+      200,
+      'Successfully returned success message',
+      successMessage,
     );
   }),
 );
