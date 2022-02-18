c1e8bcce7529c65d583e002cd197b5353f4f2f6a
nroy20 <nroy20@illinois.edu>
2022-02-17 20:10:59 -0600

    Format and lint

--- api/src/models/language.js
+++ api/src/models/language.js
@@ -1,39 +1,39 @@
 const mongoose = require('mongoose');
 
 const Course = new mongoose.Schema({
-    approved: { type: Boolean, required: true },
-    _admin_id: { type: String, required: true },
-    details: { type: CourseDetails, required: true},
+  approved: { type: Boolean, required: true },
+  _admin_id: { type: String, required: true },
+  details: { type: CourseDetails, required: true },
 });
 
 const CourseDetails = new mongoose.Schema({
-    language_name: { type: String, required: false, default: "" },
-    language_description: { type: String, required: false, default: "" },
-    population_size: { type: Number, required: false, default: "" },
+  language_name: { type: String, required: false, default: '' },
+  language_description: { type: String, required: false, default: '' },
+  population_size: { type: Number, required: false, default: '' },
 });
 
 const Unit = new mongoose.Schema({
-    _course_id: { type: String, required: true },
-    name: { type: String, required: true },
-    order: { type: Number, required: true },
-    selected: { type: Boolean, required: true },
+  _course_id: { type: String, required: true },
+  name: { type: String, required: true },
+  order: { type: Number, required: true },
+  selected: { type: Boolean, required: true },
 });
 
 const Lesson = new mongoose.Schema({
-    _course_id: { type: String, required: true },
-    _unit_id: { type: String, required: true },
-    name: { type: String, required: true },
-    order: { type: Number, required: true },
-    selected: { type: Boolean, required: true },    
-    vocab: { type: [Vocab], required: true, default: [] },
+  _course_id: { type: String, required: true },
+  _unit_id: { type: String, required: true },
+  name: { type: String, required: true },
+  order: { type: Number, required: true },
+  selected: { type: Boolean, required: true },
+  vocab: { type: [Vocab], required: true, default: [] },
 });
 
 const Vocab = new mongoose.Schema({
-    order: { type: Number, required: true },
-    original: { type: String, required: true },
-    translation: { type: String, required: true },
-    image: { type: String, required: false, default: "" },    
-    audio: { type: String, required: false, default: "" },
+  order: { type: Number, required: true },
+  original: { type: String, required: true },
+  translation: { type: String, required: true },
+  image: { type: String, required: false, default: '' },
+  audio: { type: String, required: false, default: '' },
 });
 
 module.exports.Course = mongoose.model('Course', Course);
