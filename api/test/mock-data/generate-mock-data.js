25d90a9ee569209a796383d5cfc992c5a52c6960
Ashay Parikh <ashayparikh22@gmail.com>
2022-02-20 22:36:34 -0600

    created script for generating fake user data

---
+++ api/test/mock-data/generate-mock-data.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const { faker } = require('@faker-js/faker');
+
+const NUM_FAKE_ENTRIES = 50;
+
+const saveDataToFile = (data, filename) => {
+    const serializedData = JSON.stringify(data, null, 2);
+    fs.writeFileSync(`${__dirname}/../${filename}`, serializedData);
+};
+
+/**
+ * Generates n number of users
+ *
+ * @param {number} n the number of users to generate
+ * @returns the generated users
+ */
+const generateUsers = (n) => {
+    const generateUser = () => ({
+        _id: faker.datatype.uuid(),
+        role: 0,
+        authID: faker.datatype.uuid(),
+        adminLanguages: [],
+        learnerLanguages: [],
+        collaboratorLanguages: [],
+    });
+
+    const users = Array(n).fill(null).map(generateUser);
+    return users;
+};
+
+
+// Change this to change what data is generated
+const generate = () => {
+    const userData = generateUsers(NUM_FAKE_ENTRIES);
+
+    saveDataToFile(userData, './mock-data/users.json');
+};
+
+generate();
