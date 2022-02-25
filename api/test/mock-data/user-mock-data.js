57a50f8ce1f2a3483ab88443cf0080ee8c7e90fb
Jessica Singh <jessicasingh@outlook.com>
2022-02-24 20:33:57 -0600

    Wrote tests for user post endpoint

--- api/test/mock-data/user-mock-data.js
+++ api/test/mock-data/user-mock-data.js
@@ -10,3 +10,34 @@ module.exports.POST_SIMPLE_USER_EXPECTED = {
   learnerLanguages: [],
   collaboratorLanguages: [],
 };
+
+module.exports.POST_WRONG_USER_NO_AUTH_ID = {
+  role: 0,
+  adminLanguages: [],
+  learnerLanguages: [],
+  collaboratorLanguages: [],
+};
+
+module.exports.POST_WRONG_USER_ADMIN = {
+  role: 1,
+  authID: 'ba32cb26-2020-4fbc-b77d-34ea6b0790a6',
+  adminLanguages: [],
+  learnerLanguages: [],
+  collaboratorLanguages: [],
+};
+
+module.exports.POST_WRONG_USER_ADDITIONAL_FIELDS = {
+  role: 0,
+  authID: 'ba32cb26-2020-4fbc-b77d-34ea6b0790a6',
+  adminLanguages: [],
+  learnerLanguages: [],
+  collaboratorLanguages: [],
+  randomLanguages: [],
+};
+
+module.exports.POST_WRONG_USER_NO_ROLE = {
+  authID: 'ba32cb26-2020-4fbc-b77d-34ea6b0790a6',
+  adminLanguages: [],
+  learnerLanguages: [],
+  collaboratorLanguages: [],
+};
--- api/test/routes/user.test.js
+++ api/test/routes/user.test.js
@@ -5,6 +5,9 @@ const db = require('../utils/db');
 const {
   POST_SIMPLE_USER,
   POST_SIMPLE_USER_EXPECTED,
+  POST_WRONG_USER_ADMIN,
+  POST_WRONG_USER_NO_AUTH_ID,
+  POST_WRONG_USER_ADDITIONAL_FIELDS,
 } = require('../mock-data/user-mock-data');
 
 // This block tests the POST /user/ endpoint.
@@ -24,14 +27,60 @@ describe('POST /user/ ', () => {
     expect(1).toEqual(1);
   });
 
-  // test('API should create user', async () => {
-  //   const body = POST_SIMPLE_USER;
-  //   const response = await request(app).post('/user/').send(body);
+  test('API should create user', async () => {
+    const body = POST_SIMPLE_USER;
+    const response = await request(app).post('/user/').send(body);
 
-  //   const message = response.body.message;
-  //   const result = _.omit(response.body.result, ['_id', '__v']);
+    const message = response.body.message;
+    const result = _.omit(response.body.result, ['_id', '__v']);
 
-  //   expect(message).toEqual('Successfully created a new user');
-  //   expect(result).toEqual(POST_SIMPLE_USER_EXPECTED);
-  // });
+    expect(message).toEqual('Successfully created a new user');
+    expect(result).toEqual(POST_SIMPLE_USER_EXPECTED);
+  });
+
+  test('HTTP response status code', async () => {
+    const body = POST_SIMPLE_USER;
+    const response = await request(app).post('/user/').send(body);
+
+    expect(response.status).toBe(200);
+  });
+
+  test('User Role is always 0', async () => {
+    const body = POST_WRONG_USER_ADMIN;
+    const response = await request(app).post('/user/').send(body);
+
+    const message = response.body.message;
+    const result = _.omit(response.body.result, ['_id', '__v']);
+
+    expect(message).toEqual('Successfully created a new user');
+    expect(result).toEqual(POST_SIMPLE_USER_EXPECTED);
+  });
+
+  test('No auth id results in error', async () => {
+    const body = POST_WRONG_USER_NO_AUTH_ID;
+    const response = await request(app).post('/user/').send(body);
+    expect(response.status).toBeGreaterThanOrEqual(400);
+  });
+
+  test('No role still creates a new user', async () => {
+    const body = POST_WRONG_USER_NO_ROLE;
+    const response = await request(app).post('/user/').send(body);
+
+    const message = response.body.message;
+    const result = _.omit(response.body.result, ['_id', '__v']);
+
+    expect(message).toEqual('Successfully created a new user');
+    expect(result).toEqual(POST_SIMPLE_USER_EXPECTED);
+  });
+
+  test('Additional fields still create a new user', async () => {
+    const body = POST_WRONG_USER_ADDITIONAL_FIELDS;
+    const response = await request(app).post('/user/').send(body);
+
+    const message = response.body.message;
+    const result = _.omit(response.body.result, ['_id', '__v']);
+
+    expect(message).toEqual('Successfully created a new user');
+    expect(result).toEqual(POST_SIMPLE_USER_EXPECTED);
+  });
 });
