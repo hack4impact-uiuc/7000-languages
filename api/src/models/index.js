947b3ee3234a76514fe913ea8c94c3300abb99e5
nroy20 <55101987+nroy20@users.noreply.github.com>
2022-02-24 19:58:40 -0600

    Create Language Schemas (#25)
    * Create language schemas (Course, CourseDescription, Unit, Lesson, Vocab)
    
    * Format and lint
    
    * Replace home.js test with empty placeholder
    
    * Fix last test by changing order of code
    
    * Switch order of schema to fix failing test
    
    * Add index of 1 to order
    
    Co-authored-by: Jessica Singh <jessicasingh@outlook.com>

--- api/src/models/index.js
+++ api/src/models/index.js
@@ -1,7 +1,17 @@
 const { User } = require('./user');
+const { Course } = require('./language');
+const { CourseDetails } = require('./language');
+const { Unit } = require('./language');
+const { Lesson } = require('./language');
+const { Vocab } = require('./language');
 
 const models = {
   User,
+  Course,
+  CourseDetails,
+  Unit,
+  Lesson,
+  Vocab,
 };
 
 module.exports = {
