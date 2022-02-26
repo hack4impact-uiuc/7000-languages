1886c38995c85c7f5116d747e19b74e4ff0363d9
Ashay Parikh <ashayparikh22@gmail.com>
2022-02-25 23:31:45 -0600

    setting up redux middleware

--- client/src/api/axios-config.js
+++ client/src/api/axios-config.js
@@ -3,16 +3,44 @@ import Constants from 'expo-constants'
 
 const API_PORT = Constants.manifest.extra.developmentApiPort
 
-const uri = `http://${Constants.manifest.debuggerHost
+// Source: https://stackoverflow.com/questions/47417766/calling-locally-hosted-server-from-expo-app/70964774
+const BASE_URL = `http://${Constants.manifest.debuggerHost
   .split(':')
-  .shift()}:${API_PORT}`
-
-const BASE_URL = uri
+  .shift()}:${API_PORT}`;
 
 // The configured axios instance to be exported
 const instance = axios.create({
   baseURL: BASE_URL,
   validateStatus: () => true,
 })
+
+let cachedJWTToken = null;
+
+export const setToken = (token) => {
+  cachedJWTToken = token;
+}
+
+/**
+ * Appends an authorization header to the request
+ */
+const addAuthHeader = async (config) => {
+  const updatedConfig = config;
+
+  // Add JWT Token to header
+  if (cachedJWTToken)
+    updatedConfig.headers.Authorization = `Bearer ${cachedJWTToken}`;
+
+  return updatedConfig;
+};
+
+/**
+ * Called when a request returns an error.
+ * Set the token to null in case it was caused by an old auth token.
+ */
+const onRequestError = (error) => {
+  return Promise.reject(error);
+};
+
+instance.interceptors.request.use(onRequestError, addAuthHeader);
 
 export default instance
