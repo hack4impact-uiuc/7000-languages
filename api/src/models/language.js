91740909c9c93590296db541b270c84e5c6c51c9
nroy20 <nroy20@illinois.edu>
2022-02-28 21:23:13 -0600

    Lint and format

--- api/src/models/language.js
+++ api/src/models/language.js
@@ -4,8 +4,8 @@ const CourseDetails = new mongoose.Schema({
   name: { type: String, required: false, default: '' },
   description: { type: String, required: false, default: '' },
   translated_language: { type: String, required: true, default: 'English' },
-  location: { type: String, required: true, default: ''},
-  iso: { type: String, required: true }
+  location: { type: String, required: true, default: '' },
+  iso: { type: String, required: true },
 });
 
 const Course = new mongoose.Schema({
@@ -19,18 +19,18 @@ const Unit = new mongoose.Schema({
   name: { type: String, required: true },
   _order: { type: Number, required: true, index: true, unique: true },
   selected: { type: Boolean, required: true },
-  description: { type: String, required: true, default: '' }
+  description: { type: String, required: true, default: '' },
 });
 
-Unit.index({ _course_id: 1, _order: 1});
+Unit.index({ _course_id: 1, _order: 1 });
 
 const Vocab = new mongoose.Schema({
-  _order: { type: Number, required: true, index: true, unique: true},
+  _order: { type: Number, required: true, index: true, unique: true },
   original: { type: String, required: true },
   translation: { type: String, required: true },
-  image: { type: String, required: false, default: ''},
-  audio: { type: String, required: false, default: ''},
-  notes: { type: string, required: true, default: ''},
+  image: { type: String, required: false, default: '' },
+  audio: { type: String, required: false, default: '' },
+  notes: { type: String, required: true, default: '' },
 });
 
 Vocab.index({ _order: 1 });
@@ -42,7 +42,7 @@ const Lesson = new mongoose.Schema({
   _order: { type: Number, required: true, index: true, unique: true },
   selected: { type: Boolean, required: true },
   vocab: { type: [Vocab], required: true, default: [] },
-  description: { type: String, required: true, default: ''}
+  description: { type: String, required: true, default: '' },
 });
 
 Lesson.index({ _course_id: 1, _unit_id: 1, _order: 1 });
