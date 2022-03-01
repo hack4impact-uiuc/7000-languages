8a4b2f59b11f8e439e87bb30e5eb091f6aa24024
nroy20 <nroy20@illinois.edu>
2022-02-28 21:21:42 -0600

    Updated fields to included required/defaults

--- api/src/models/language.js
+++ api/src/models/language.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 const CourseDetails = new mongoose.Schema({
   name: { type: String, required: false, default: '' },
   description: { type: String, required: false, default: '' },
-  translated_language: { type: String },
-  iso: { type: String }
+  translated_language: { type: String, required: true, default: 'English' },
+  location: { type: String, required: true, default: ''},
+  iso: { type: String, required: true }
 });
 
 const Course = new mongoose.Schema({
@@ -18,17 +19,18 @@ const Unit = new mongoose.Schema({
   name: { type: String, required: true },
   _order: { type: Number, required: true, index: true, unique: true },
   selected: { type: Boolean, required: true },
-  description: { type: String }
+  description: { type: String, required: true, default: '' }
 });
 
 Unit.index({ _course_id: 1, _order: 1});
 
 const Vocab = new mongoose.Schema({
-  _order: { type: Number, required: true, index: true},
+  _order: { type: Number, required: true, index: true, unique: true},
   original: { type: String, required: true },
   translation: { type: String, required: true },
-  image: { type: String, required: false, default: '' },
-  audio: { type: String, required: false, default: '' },
+  image: { type: String, required: false, default: ''},
+  audio: { type: String, required: false, default: ''},
+  notes: { type: string, required: true, default: ''},
 });
 
 Vocab.index({ _order: 1 });
@@ -37,10 +39,10 @@ const Lesson = new mongoose.Schema({
   _course_id: { type: String, required: true, index: true, unique: true },
   _unit_id: { type: String, required: true, index: true },
   name: { type: String, required: true },
-  _order: { type: Number, required: true, index: true },
+  _order: { type: Number, required: true, index: true, unique: true },
   selected: { type: Boolean, required: true },
   vocab: { type: [Vocab], required: true, default: [] },
-  description: { type: String}
+  description: { type: String, required: true, default: ''}
 });
 
 Lesson.index({ _course_id: 1, _unit_id: 1, _order: 1 });
