30490a642b20e1e040efe4fda5f0b3a8315daa3c
Jessica Singh <86633231+jessicasingh7@users.noreply.github.com>
2022-03-02 20:32:47 -0600

    Wrote tests for user post endpoint (#77)
    * Wrote tests for user post endpoint
    
    * Added one more test and changed 1 existing test
    
    * Added correct mock users
    
    * Fixed NO_ROLE test and added Duplicate authIDs test
    
    * Fixed NO_ROLE test and no authID test
    
    * Fixed Duplicate authIDs
    
    * Made authID field unique
    
    * Changed mock data id values
    
    * Check if user already exists in POST endpoint
    
    * replaced hard-coded roles with enum constants
    
    * Corrected POST_USER_ONE_LESS_FIELD mock data
    
    * updated return info for post request with existing auth id
    
    * imported constant
    
    * imported constant differently
    
    * imported constant even more differently
    
    * stopped returning authid data on sendresponse()
    
    * added temps
    
    * waaaaaat
    
    * vscode is gaslighting me
    
    Co-authored-by: nroy20 <nroy20@illinois.edu>

--- api/test/routes/user.test.js
+++ api/test/routes/user.test.js
@@ -5,6 +5,11 @@ const db = require('../utils/db');
 const {
   POST_SIMPLE_USER,
   POST_SIMPLE_USER_EXPECTED,
+  POST_USER_ADMIN,
+  POST_WRONG_USER_NO_AUTH_ID,
+  POST_USER_ADDITIONAL_FIELDS,
+  POST_USER_ONE_LESS_FIELD,
+  POST_WRONG_USER_NO_ROLE,
 } = require('../mock-data/user-mock-data');
 
 // This block tests the POST /user/ endpoint.
@@ -24,14 +29,88 @@ describe('POST /user/ ', () => {
     expect(1).toEqual(1);
   });
 
-  // test('API should create user', async () => {
-  //   const body = POST_SIMPLE_USER;
-  //   const response = await request(app).post('/user/').send(body);
+  test('API should create user', async () => {
+    const body = POST_SIMPLE_USER;
+    const response = await request(app).post('/user/').send(body);
 
-  //   const message = response.body.message;
-  //   const result = _.omit(response.body.result, ['_id', '__v']);
+    const message = response.body.message;
+    const result = _.omit(response.body.result, ['_id', '__v']);
 
-  //   expect(message).toEqual('Successfully created a new user');
-  //   expect(result).toEqual(POST_SIMPLE_USER_EXPECTED);
-  // });
+    expect(message).toEqual('Successfully created a new user');
+    expect(result).toEqual(POST_SIMPLE_USER_EXPECTED);
+  });
+
+  test('HTTP response status code', async () => {
+    const body = POST_SIMPLE_USER;
+    const response = await request(app).post('/user/').send(body);
+
+    expect(response.status).toBe(200);
+  });
+
+  test('User Role is always 0', async () => {
+    const body = POST_USER_ADMIN;
+    const response = await request(app).post('/user/').send(body);
+
+    const message = response.body.message;
+    const result = _.omit(response.body.result, ['_id', '__v']);
+
+    expect(message).toEqual('Successfully created a new user');
+    expect(result).toEqual(POST_SIMPLE_USER_EXPECTED);
+  });
+
+  test('No auth id results in error', async () => {
+    const body = POST_WRONG_USER_NO_AUTH_ID;
+    const response = await request(app).post('/user/').send(body);
+
+    expect(response.status).toBeGreaterThanOrEqual(400);
+  });
+
+  test('No role still creates a new user', async () => {
+    const body = POST_WRONG_USER_NO_ROLE;
+    const response = await request(app).post('/user/').send(body);
+
+    const message = response.body.message;
+    const result = _.omit(response.body.result, ['_id', '__v']);
+
+    expect(message).toEqual('Successfully created a new user');
+    expect(result).toEqual(POST_SIMPLE_USER_EXPECTED);
+  });
+
+  test('Additional fields still create a new user', async () => {
+    const body = POST_USER_ADDITIONAL_FIELDS;
+    const response = await request(app).post('/user/').send(body);
+
+    const message = response.body.message;
+    const result = _.omit(response.body.result, ['_id', '__v']);
+
+    expect(message).toEqual('Successfully created a new user');
+    expect(result).toEqual(POST_SIMPLE_USER_EXPECTED);
+  });
+
+  test('One less field still create a new user', async () => {
+    const body = POST_USER_ONE_LESS_FIELD;
+    const response = await request(app).post('/user/').send(body);
+
+    const message = response.body.message;
+    const result = _.omit(response.body.result, ['_id', '__v']);
+
+    expect(message).toEqual('Successfully created a new user');
+    expect(result).toEqual(POST_SIMPLE_USER_EXPECTED);
+  });
+
+  test('Duplicate authIDs cannot exist', async () => {
+    const body_1 = POST_SIMPLE_USER;
+    const response_1 = await request(app).post('/user/').send(body_1);
+
+    const message = response_1.body.message;
+    const result = _.omit(response_1.body.result, ['_id', '__v']);
+
+    expect(message).toEqual('Successfully created a new user');
+    expect(result).toEqual(POST_SIMPLE_USER_EXPECTED);
+
+    const body_2 = POST_SIMPLE_USER;
+    const response_2 = await request(app).post('/user/').send(body_2);
+
+    expect(response_2.status).toEqual(202);
+  });
 });
