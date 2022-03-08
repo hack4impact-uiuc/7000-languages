1087524a0eb931b927f5408b8647d09b43c4152d
nroy20 <nroy20@illinois.edu>
2022-03-07 20:59:13 -0600

    put doesn't work

---
+++ api/src/api/language.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const { errorWrap } = require('../middleware');
+const { sendResponse } = require('../utils/response');
+const { models } = require('../models/index.js');
+
+/**
+ * put
+ */
+
+ router.put(
+    '/language/course/:id',
+    errorWrap(async (req, res) => {
+      const updates = req.body;
+      const course = await models.Course.findOne({_id: req.params.id});
+      if (updates.approved) {
+        course.approved = updates.approved;
+      }
+      if (updates.admin_id) {
+        course.admin_id = updates.admin_id;
+      }
+      if (updates.details) {
+        course.details = updates.details;
+      }
+      await course.save();
+      return sendResponse(res, 200, 'Successfully updated course', course);
+    }),
+  );
+
+module.exports = router;
