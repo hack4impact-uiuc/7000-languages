0cfd7f24d891a10d4e8a7b1d3d257ef3e5e5781d
Jessica Singh <jessicasingh@outlook.com>
2022-03-08 15:24:37 -0600

    Yarn lint after adding import statement

--- api/src/api/language.js
+++ api/src/api/language.js
@@ -3,45 +3,49 @@ const router = express.Router();
 const { errorWrap } = require('../middleware');
 const { sendResponse } = require('../utils/response');
 const { models } = require('../models/language.js');
-const { requireAuthentication } = require("../middleware/authentication");
-
+const { requireAuthentication } = require('../middleware/authentication');
+const _ = require('lodash');
 /**
  * Creates a new course in the database
  *
  * @param {newUser} New course
  * @returns a new course under the given language
  */
- router.post(
-    '/language/course',
-    requireAuthentication,
-    // const user = req.user;
-    // user.authID
-    
-    // const newCourse = new models.Course(someCourseData)
-    // await newCourse.save();
-    errorWrap(async (req, res) => {
-      const user = req.user;
+router.post(
+  '/language/course',
+  requireAuthentication,
+  // const user = req.user;
+  // user.authID
 
-      const newCourse = new models.Course({
-        approved: true,
-        admin_id: user.authID,
-        details: { 
-            name: req.body.name,
-            description: req.body.description,
-            translated_language: req.body.translated_language,
-            location: req.body.location,
-            iso: req.body.iso, 
-        },
-      });
+  // const newCourse = new models.Course(someCourseData)
+  // await newCourse.save();
+  errorWrap(async (req, res) => {
+    const user = req.user;
 
-      user.adminLanguages.push(newCourse._id);
+    const newCourse = new models.Course({
+      approved: true,
+      admin_id: user.authID,
+      details: {
+        name: req.body.name,
+        description: req.body.description,
+        translated_language: req.body.translated_language,
+        location: req.body.location,
+        iso: req.body.iso,
+      },
+    });
 
-      await newCourse.save();
-      let newResult = newCourse.toJSON();
-      newResult = _.omit(newResult, ['admin_id']);
-      return sendResponse(res, 200, 'Successfully created a new course', newResult);
-    }),
-  );
-  
-  module.exports = router;
-  
+    user.adminLanguages.push(newCourse._id);
+
+    await newCourse.save();
+    let newResult = newCourse.toJSON();
+    newResult = _.omit(newResult, ['admin_id']);
+    return sendResponse(
+      res,
+      200,
+      'Successfully created a new course',
+      newResult,
+    );
+  }),
+);
+
+module.exports = router;
