3a11f3bd93420009d5f08e4c770e3028e20509f4
ishaansharma <ishaan02@hotmail.com>
2022-03-07 20:30:28 -0600

    a

--- client/src/theme/images.js
+++ client/src/theme/images.js
@@ -3,7 +3,7 @@ import { Asset } from 'expo-asset'
 const images = {
   // logo_sm: require('../../assets/images/logo-sm-black.png'),
   // logo_lg: require('../../assets/images/logo-lg-black.png'),
-  background_landing: require('../../assets/images/landing-background.png')
+  background_landing: require('../../assets/images/landing-background.png'),
   landing_logo: require('../../assets/images/landing-logo.svg')
 
 }
