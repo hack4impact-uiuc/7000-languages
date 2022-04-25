cb4c4aa46ac671298fad7c88b9401a9c12da5f48
Ashay Parikh <ashayparikh22@gmail.com>
2022-04-25 12:00:55 -0500

    working on code

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -23,3 +23,43 @@ export const getUser = async () => {
   if (!res?.data?.success) throw new Error(res?.data?.message)
   return res.data
 }
+
+export const getCourse = async (course_id) => {
+  const requestString = `/language/course/${course_id}`
+  const res = await instance.get(requestString)
+
+  if (!res?.data?.success) throw new Error(res?.data?.message)
+  return res.data
+}
+
+export const getCourse = async (courseID) => {
+  const requestString = `/course/${courseID}`
+  const res = await instance.get(requestString)
+
+  if (!res?.data?.success) throw new Error(res?.data?.message)
+  return res.data
+}
+
+// TODO: add create unit
+
+// export const getUnit = async (courseID) => {
+//   const requestString = `/course/${courseID}`
+//   const res = await instance.get(requestString)
+
+//   if (!res?.data?.success) throw new Error(res?.data?.message)
+//   return res.data
+// }
+
+// getUnit
+// updateUnits
+
+// getLesson
+// updateLessons
+// createLesson
+
+// createVocabItem
+// updateVocabItem
+
+
+
+
