60572bf5f4ac0f41ccf1569dbe6812e226959755
Jessica Singh <86633231+jessicasingh7@users.noreply.github.com>
2022-04-28 20:24:11 -0500

    Get endpoint language audio (#165)
    Get audio endpoint using aws

--- api/src/api/language/audio.js
+++ api/src/api/language/audio.js
@@ -3,14 +3,46 @@ const router = express.Router();
 const { errorWrap } = require('../../middleware');
 const { sendResponse } = require('../../utils/response');
 const { models } = require('../../models/index.js');
+const { uploadFile, downloadFile } = require('../../utils/aws/s3.js');
 const { requireAuthentication } = require('../../middleware/authentication');
-const { uploadFile } = require('../../utils/aws/s3');
-const { ERR_MISSING_OR_INVALID_DATA } = require('../../utils/constants');
-const { checkIds } = require('../../utils/languageHelper');
 const {
   requireLanguageAuthorization,
 } = require('../../middleware/authorization');
+const { ERR_MISSING_OR_INVALID_DATA } = require('../../utils/constants');
+const { checkIds } = require('../../utils/languageHelper');
 const fs = require('fs');
+
+router.get(
+  '/:course_id/:unit_id/:lesson_id/:vocab_id/',
+  requireAuthentication,
+  requireLanguageAuthorization,
+  errorWrap(async (req, res) => {
+    const { course_id, unit_id, lesson_id, vocab_id } = req.params;
+
+    const isValid = await checkIds({ course_id, unit_id, lesson_id, vocab_id });
+
+    if (!isValid) {
+      return sendResponse(res, 400, ERR_MISSING_OR_INVALID_DATA);
+    }
+
+    // Open a stream from the S3 bucket
+    const s3Stream = downloadFile(
+      `files/${course_id}/${unit_id}/${lesson_id}/${vocab_id}/audio`,
+    ).createReadStream();
+
+    // Setup callbacks for stream error and stream close
+    s3Stream
+      .on('error', (err) => {
+        res.json(`S3 Error:${err}`);
+      })
+      .on('close', () => {
+        res.end();
+      });
+
+    // Pipe the stream to the client
+    s3Stream.pipe(res);
+  }),
+);
 
 router.post(
   '/:course_id/:unit_id/:lesson_id/:vocab_id',
