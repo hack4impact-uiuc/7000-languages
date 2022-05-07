49bacac71d80985b98c3de57f1c00c7721212827
Ashay Parikh <ashayparikh22@gmail.com>
2022-05-07 05:40:13 +0000

    App is Prepared for Beta Testing (#177)
    * Created Gulp File for creating build application (not being used)
    * Increased EB request file size in order to prevent 413 error code 
    * Created Procfile for setting Elastic Beanstalk (EB) run command
    * Moved client environment variables to app.config.js
    * Added AWS and EB CLI

--- api/src/api/index.js
+++ api/src/api/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => res.json('API working!'));
+router.get('/', (req, res) =>
+  res.json(`API Working, running ${process.env.NODE_ENV}`),
+);
 // Put all routes here
 router.use('/user', require('./user'));
 router.use('/language', require('./language'));
