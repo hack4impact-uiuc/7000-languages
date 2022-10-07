7e268cf331fea5304d9ceed08ae3a1b15b93be38
Rustom Ichhaporia <45826892+rustom@users.noreply.github.com>
2022-10-06 21:32:11 -0500

    Add simple secondary post

--- api/src/api/language/course.js
+++ api/src/api/language/course.js
@@ -9,7 +9,11 @@ const {
 } = require('../../middleware/authorization');
 const _ = require('lodash');
 const { ERR_NO_COURSE_DETAILS } = require('../../utils/constants');
-const { patchDocument } = require('../../utils/languageHelper');
+const { getNumUnitsInCourse, patchDocument } = require('../../utils/languageHelper');
+const {
+  POST_SIMPLE_UNIT
+} = require('../../utils/example-data.js');
+
 /**
  * Does a patch update a single course in the database, meaning
  * it makes changes to parts of the course specified in the request.
@@ -32,6 +36,57 @@ router.patch(
   }),
 );
 
+async function generateDefaultUnits(course_id) {
+  const temp = POST_SIMPLE_UNIT;
+  temp['_course_id'] = course_id;
+  console.log('the fucking course id' + course_id);
+
+  // errorWrap(async (req, res) => {
+  const unitData = temp;//req.body;
+  console.log('fuck me');
+
+  // if (unitData.name === '' || unitData.description === '') {
+  //   return sendResponse(
+  //     res,
+  //     400,
+  //     'You are missing a unit name and/or description. Please try again.',
+  //   );
+  // }
+
+  // const course_id = unitData._course_id;
+
+  const order = await getNumUnitsInCourse(course_id);
+
+  const newUnit = new models.Unit({
+    _course_id: course_id,
+    name: unitData.name,
+    _order: order,
+    selected: unitData.selected,
+    description: unitData.description,
+  });
+
+  await newUnit.save();
+  let newResult = newUnit.toJSON();
+  newResult.num_lessons = 0;
+
+  console.log(newResult);
+
+  // return sendResponse(res, 200, 'Successfully created a new unit', newResult);
+  // })
+
+  // const response = await withAuthentication(
+  //   request(app).post('/language/unit').send(temp)
+  // );
+
+
+  // const message = response.body.message;
+  // const result = omitDeep(response.body.result, '__v', '_id');
+  // expect(response.status).toBe(200);
+  // expect(message).toEqual('Successfully created a new unit');
+  // expect(result).toEqual(POST_EXPECTED_UNIT);
+  return
+}
+
 /**
  * Creates a new course in the database
  */
@@ -49,6 +104,23 @@ router.post(
       admin_id: user.authID,
       details: courseData.details,
     });
+
+    // const temp = POST_SIMPLE_UNIT;
+    // temp['name'] = 'asdf';
+    // console.log(temp);
+    // console.log('ADMINID: ' + user.authID);
+
+    // const newCourse2 = new models.Course(
+    //   POST_SIMPLE_UNIT
+    //   // how can i input the POST_SIMPLE_UNIT data without unwrapping every element
+    // );
+    // newCourse2
+    // await newCourse2.save();
+    // let newResult = newCourse2.toJSON();
+
+    newCourse.details.description = 'bruh';
+
+    generateDefaultUnits(newCourse._id);
 
     await newCourse.save();
     let newResult = newCourse.toJSON();
---
+++ api/src/utils/example-data.js
@@ -0,0 +1,6 @@
+module.exports.POST_SIMPLE_UNIT = {
+  _course_id: '',
+  name: 'Huge win for rustom',
+  selected: true,
+  description: 'rustom\'s description',
+};
