c18e788d7fc812a8d0fc4175b95e398cd47396d1
eeshabarua <eesha.barua@gmail.com>
2022-10-18 15:49:34 -0500

    Create JSON Model, LanguageData component

---
+++ client/src/components/LanguageData/translations.js
@@ -0,0 +1,124 @@
+const translations = {
+  en: {
+    dict: {
+      welcome: 'Welcome!',
+      vocabItems: 'Vocabulary Items',
+      discard: 'Discard',
+      ok: 'OK',
+      unitSingle: '1 Unit',
+      unitPlural: 'Units',
+      lessonSingle: 'Lesson',
+      lessonPlural: 'Lessons',
+      loading: 'Loading...',
+      success: 'Success!',
+      languageDescription: 'Language Description',
+      ISOCode: 'ISO Code',
+      glottoCode: 'Glotto Code',
+      termsAndConditions: 'Terms and Conditions',
+      suggestion: 'Suggestion',
+      courseHome: 'Course Home',
+      vocab: 'Vocab',
+      itemSingle: 'Item',
+      itemPlural: 'Items',
+      selectedLessons: 'Selected Lessons',
+      unselectedLessons: 'Unselected Lessons',
+      selectedUnits: 'Selected Units',
+      unselectedUnits: 'Unselected Units',
+      moreInfo: 'Additional Information',
+    },
+    actions: {
+      searchCourses: 'Search Courses',
+      logOut: 'Log Out',
+      becomeContributor: 'Become a contributor',
+      manageUnits: 'Manage Units',
+      notLeave: "Don't leave",
+      saveChanges: 'Save Changes',
+      recordAudio: 'Record Audio',
+      applyNow: 'Apply Now',
+      myCourses: 'My Courses',
+      required: 'Required.',
+      addCustomLesson: 'Add Custom Lesson',
+      createLessonSingle: 'Create Lesson',
+      addCustomUnit: 'Add Custom Unit',
+      createUnit: 'Create Unit',
+      goBack: 'Go Back',
+      continueGoogle: 'Continue with Google',
+      login: 'Login',
+      loginToApp: 'Login to App',
+      createLessonPlural: 'Create Lessons',
+      createUnitPlural: 'Create Units',
+      goToDetails: 'Go to Details',
+      manageLessons: 'Manage Lessons',
+      captureNewImage: 'Capture a New Image',
+      selectPictureLibrary: 'Select from Picture Library',
+      takeWithCamera: 'Take With Camera',
+      cancel: 'Cancel',
+      updateImage: 'Update Image',
+      removeImage: 'Remove Image',
+      addImage: 'AddImage',
+      editVocabItem: 'Edit Vocab Item',
+      addVocabItem: 'Add a Vocab Item',
+    },
+    dialogue: {
+      notLearnerPrompt: "Looks like you aren't a learner in any courses yet.",
+      ourMission:
+        'Our mission is to help communities teach, learn, and sustain their endangered languages.',
+      supportRevitalization:
+        'We’d love to support your revitalization efforts.',
+      setDescriptionPrompt: 'You currently have not set a description.',
+      unsavedChangesPrompt:
+        'You have unsaved changes. Are you sure to discard them and leave the screen?',
+      discardChangesPrompt: 'Discard Changes?',
+      requiredField: 'This field is required',
+      errorCaught: 'useErrorWrap(): error caught:',
+      indigenousLanguagePrompt:
+        'Do you know an indigenous language that you would like to share with the world?',
+      nameRequired: 'Name is required',
+      emailRequired: 'Email is required',
+      languageRequired: 'Language is required',
+      termsRequired: 'Terms are required',
+      applicationSuccess: 'You have succesfully submitted your application!',
+      contributorThanks: 'Thanks for your interest in contributing a language.',
+      applicationFeedback: `We hope this app will support your language revitalization
+                efforts. We have a few questions for you and we will get back to
+                you in 1 - 2 weeks. If approved, your course will appear on your
+                home page.`,
+      languageDescriptionPrompt: `Provide a 1-2 sentence description of your language and/or
+                culture. This will be shown to learners in this course.`,
+      alternativeNamesPrompt: 'Any alternative names?',
+      ISOCodePrompt: 'You can find the ISO code here',
+      glottoCodePrompt: 'You can find the Glotto code here',
+      languageOriginPrompt: 'Where is this language spoken?',
+      languagePopulationPrompt:
+        'Approximately how many people speak this language?',
+      languageLinkPrompt: 'Link to additional information about this language.',
+      agree: 'I agree to the',
+      resourcesFollowUp: `I would like a team member from 7000 Languages to follow
+                up with me about creating additional resources for my
+                language.`,
+      languageUsePermission: `By selecting this button, you have permission from the
+          community/speakers to create language learning materials.`,
+      lessonNamePrompt: 'Give your lesson a name',
+      lessonGoalsPrompt: 'What are the goals of this lesson?',
+      createUnitDescription:
+        'When creating a unit, think about how it will be used. More text here explaining what they should look for when making a unit.',
+      unitNamePrompt: 'Give your unit a name',
+      unitPurposePrompt: 'What is the purpose of this unit?',
+      details: 'Details (from ___ )',
+      landingQuote: 'To speak a language is to take on a world, a culture.',
+      selectedLessonsPrompt:
+        'These lessons will be available to your students. Drag them around to reorder them.',
+      unselectedLessonsPrompt:
+        'These lessons are not included in your course. You can still continue to edit them.',
+      selectedUnitsPrompt:
+        'These units will be available to your students. Drag them around to reorder them.',
+      unselectedUnitsPrompt:
+        'These units are not included in your course. You can still continue to edit them.',
+      itemDescriptionPrompt: 'A vocab item can be a word or phrase.',
+      moreInfoPrompt: `Use this space to give additional information about the vocab item, such
+        as grammatical and cultural information, usage, or additional
+        translations/meanings.`,
+    },
+  },
+  fr: {},
+}
