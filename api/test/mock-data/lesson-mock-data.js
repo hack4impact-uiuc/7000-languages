2f54a5f3280f77bd69b13b7ed11c6307eb1a7231
Ashay Parikh <ashayparikh22@gmail.com>
2022-11-01 22:59:25 -0500

    tests all good now?

--- api/test/mock-data/lesson-mock-data.js
+++ api/test/mock-data/lesson-mock-data.js
@@ -1018,7 +1018,7 @@ module.exports.PATCH_LESSON_SELECTED_EXPECTED = {
       translation: 'provident',
       image: ';1BU*FsGAg',
       audio: '_jQ`dG/6"r',
-      selected: false,
+      selected: true,
       notes:
         'Illum repudiandae autem impedit omnis quaerat impedit ab. Harum sit quo autem expedita. Deleniti fugiat nulla. Et ipsam temporibus sit architecto. Atque ut mollitia dolorem.',
     },
@@ -1029,7 +1029,7 @@ module.exports.PATCH_LESSON_SELECTED_EXPECTED = {
       translation: 'natus',
       image: '#V5G*yQe:u',
       audio: 'D6Oiz^nSM>',
-      selected: false,
+      selected: true,
       notes:
         'Unde optio modi. Reiciendis dolorem atque. Et hic quia et qui error et sint libero et. Sapiente eum voluptatem beatae consequatur ut rerum similique. Eligendi qui aut velit. Voluptatem quisquam ipsa quas.',
     },
@@ -1040,7 +1040,7 @@ module.exports.PATCH_LESSON_SELECTED_EXPECTED = {
       translation: 'animi',
       image: '|lr=).=)(8',
       audio: '1#Mm_>!(=4',
-      selected: false,
+      selected: true,
       notes:
         'Distinctio quia vitae quaerat numquam numquam ut et quam. Quas quas provident. Eligendi exercitationem et et dolorem ea eos ab. Ipsum quia quibusdam consequuntur error explicabo. Ipsam corporis harum temporibus omnis qui. Blanditiis est illum doloremque dicta recusandae qui molestiae.',
     },
@@ -1051,7 +1051,7 @@ module.exports.PATCH_LESSON_SELECTED_EXPECTED = {
       translation: 'recusandae',
       image: 'SU\\Y#EVi;#',
       audio: 'R[ZU9T-B^|',
-      selected: false,
+      selected: true,
       notes:
         'Beatae quo ipsum beatae voluptatem excepturi quam dolor qui. Eaque repellendus assumenda nihil voluptate voluptatibus. Qui illo qui et ut consectetur veritatis quia laudantium sunt.',
     },
@@ -1062,7 +1062,7 @@ module.exports.PATCH_LESSON_SELECTED_EXPECTED = {
       translation: 'ut',
       image: 'RjshB{_1vh',
       audio: 'w"O1w>_(3R',
-      selected: false,
+      selected: true,
       notes:
         'Voluptatem dolorum rem. Dolorem totam mollitia vel ducimus consequuntur et consequatur quidem aspernatur. Nostrum fugit blanditiis ea delectus blanditiis eum rerum magnam molestiae. Odio temporibus ullam eum est et fugiat est id facere. Non sequi quia tempore iste qui aspernatur.',
     },
@@ -1073,7 +1073,7 @@ module.exports.PATCH_LESSON_SELECTED_EXPECTED = {
       translation: 'et',
       image: 'f>AOAZ2,d{',
       audio: '<]N}3}!ai*',
-      selected: false,
+      selected: true,
       notes:
         'Id veritatis harum voluptatem. Odio commodi consequatur. Quia libero nemo et eum consequuntur debitis.',
     },
@@ -1084,7 +1084,7 @@ module.exports.PATCH_LESSON_SELECTED_EXPECTED = {
       translation: 'repudiandae',
       image: 'Avpa=v[r.;',
       audio: '|Y^fQw2=*$',
-      selected: false,
+      selected: true,
       notes:
         'Id voluptatem officia animi necessitatibus consequatur labore facilis. Exercitationem tempora culpa dolor omnis. Id enim culpa.',
     },
@@ -1095,7 +1095,7 @@ module.exports.PATCH_LESSON_SELECTED_EXPECTED = {
       translation: 'saepe',
       image: 'yMq\\?qCw1P',
       audio: '()xOAtxImF',
-      selected: false,
+      selected: true,
       notes:
         'Reiciendis officia nulla ea harum excepturi tempora et. Ea veniam consequuntur. Optio et autem suscipit quisquam voluptatem quas beatae at. Et sunt quia repudiandae ipsam consequuntur inventore aspernatur laboriosam laudantium. Vel velit ut dolorum aut dolorem nihil vel. Fuga labore fuga velit blanditiis.',
     },
@@ -1106,7 +1106,7 @@ module.exports.PATCH_LESSON_SELECTED_EXPECTED = {
       translation: 'est',
       image: '?4RJVLUy+c',
       audio: '8r]Zh\\osb`',
-      selected: false,
+      selected: true,
       notes:
         'Voluptatum laudantium ipsa aut asperiores molestiae. Dicta quos similique esse eaque magnam vero excepturi quibusdam omnis. Mollitia et dolores repellat reprehenderit repudiandae. Perspiciatis quia excepturi qui consequatur aut et.',
     },
@@ -1117,7 +1117,7 @@ module.exports.PATCH_LESSON_SELECTED_EXPECTED = {
       translation: 'alias',
       image: "UZC;;Pp'iV",
       audio: "}Rzp'[57%Y",
-      selected: false,
+      selected: true,
       notes:
         'Corrupti ex corrupti esse molestiae tenetur. Nulla itaque cumque iusto omnis dolores. Quaerat quis delectus.',
     },
@@ -1128,7 +1128,7 @@ module.exports.PATCH_LESSON_SELECTED_EXPECTED = {
       translation: 'accusantium',
       image: 'hPpbo<l<B`',
       audio: 'M7,]J$S2sx',
-      selected: false,
+      selected: true,
       notes:
         'Temporibus expedita alias commodi qui earum temporibus corporis omnis. Consequatur temporibus exercitationem rerum tenetur enim id error. Totam ad nulla aliquid rerum. Neque illum excepturi magnam veritatis maxime.',
     },
@@ -1139,7 +1139,7 @@ module.exports.PATCH_LESSON_SELECTED_EXPECTED = {
       translation: 'incidunt',
       image: 'tDcbWec<X;',
       audio: '[v5yZKx32@',
-      selected: false,
+      selected: true,
       notes:
         'Ut aliquam facilis est et aut quibusdam eius qui ab. Iure et omnis dolorum. Possimus voluptatem esse. Vel praesentium repellendus aliquam consequatur officiis amet. Corporis dicta mollitia dolorem.',
     },
@@ -1150,7 +1150,7 @@ module.exports.PATCH_LESSON_SELECTED_EXPECTED = {
       translation: 'ut',
       image: 'biy<=!Rm{g',
       audio: '(!Uq4Tn-Ql',
-      selected: false,
+      selected: true,
       notes:
         'Dolorem iusto maxime iure aut reiciendis. Tempora exercitationem quidem harum aliquam est necessitatibus nisi. Recusandae voluptatum nihil sit provident soluta necessitatibus nisi adipisci dignissimos. Aut sapiente eum. Voluptatibus sit qui natus rerum est sunt sunt quidem. Neque hic minus.',
     },
@@ -1161,7 +1161,7 @@ module.exports.PATCH_LESSON_SELECTED_EXPECTED = {
       translation: 'quo',
       image: 'hezj+J!flk',
       audio: '>8BGa^M=B8',
-      selected: false,
+      selected: true,
       notes:
         'Id autem expedita et perspiciatis. Accusantium nesciunt aut at consectetur et quidem officia magni possimus. Provident voluptatem magnam nihil consequatur. Eos est autem rerum et unde cum.',
     },
@@ -1172,7 +1172,7 @@ module.exports.PATCH_LESSON_SELECTED_EXPECTED = {
       translation: 'temporibus',
       image: 't1v3So\\e1W',
       audio: 'aB|F^0An/U',
-      selected: false,
+      selected: true,
       notes:
         'Pariatur fuga occaecati corporis placeat nobis rerum et et et. Error qui veritatis nam. Quae qui nobis. Ratione rerum cupiditate optio esse. Voluptatem velit ut cupiditate dolorem sit amet nam nostrum unde.',
     },
