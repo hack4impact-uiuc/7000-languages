6081ce3bb73dcd3011696a47fafc3f9c7a18534b
Ashay Parikh <ashayparikh22@gmail.com>
2022-11-17 01:25:05 -0600

    swap client id

--- client/app.config.js
+++ client/app.config.js
@@ -3,7 +3,7 @@ export default ({ config }) => {
   return {
     ...config,
     extra: {
-      apiURL: 'http://7000-languages-api-prod-env.eba-9hbad6za.us-east-1.elasticbeanstalk.com', // Comment out if you want to connect to the local api
+      // apiURL: 'http://7000-languages-api-prod-env.eba-9hbad6za.us-east-1.elasticbeanstalk.com', // Comment out if you want to connect to the local api
       apiDevelopmentPort: 3000,
       expoClientId:
         '1534417123-rirmc8ql9i0jqrqchojsl2plf5c102j6.apps.googleusercontent.com',
