f9a6934a7f1b7702cb1021fa008fa066809edccb
Ashay Parikh <ashayparikh22@gmail.com>
2022-11-18 02:18:31 -0600

    push correct app.config

--- client/app.config.js
+++ client/app.config.js
@@ -3,12 +3,11 @@ export default ({ config }) => {
   return {
     ...config,
     extra: {
-      apiURL:
-        'http://7000-languages-api-prod-env.eba-9hbad6za.us-east-1.elasticbeanstalk.com', // Comment out if you want to connect to the local api
+      apiURL: '', // Comment out if you want to connect to the local api
       apiDevelopmentPort: 3000,
       expoClientId:
-        '1534417123-kdiotii3qddj0kumdchnrv870u9c0ihl.apps.googleusercontent.com',
-      clientSecret: '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',
+        '1534417123-rirmc8ql9i0jqrqchojsl2plf5c102j6.apps.googleusercontent.com',
+      clientSecret: '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',
     },
   }
 }
